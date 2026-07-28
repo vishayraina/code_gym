e0eb2edc24f0effe0dbb3546d9b5f6fad4402014
Vishay Raina <46987787+vishayraina@users.noreply.github.com>
2026-07-28 21:52:58 +0530

    Time: 486 ms (9.49%), Space: 19.6 MB (94.05%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -2,28 +2,25 @@ class Solution:
     def minWindow(self, s: str, t: str) -> str:
         def is_valid(hmap,tmap):
             for c in tmap:
-                if c not in hmap or hmap[c] < tmap[c]:
+                if tmap[c] > hmap.get(c, 0):
                     return False
             return True
-
+    
         tmap = {}
+        hmap = {}
         for c in t:
             tmap[c] = tmap.get(c, 0) + 1
+
         l = 0
-        hmap = {}
-        minlen = float("inf")
-        res = [0,0]
+        res, sol = float("inf"), [0,0]
         for r in range(len(s)):
-            if s[r] in tmap:
-                hmap[s[r]] = hmap.get(s[r], 0) + 1
+            hmap[s[r]] = hmap.get(s[r], 0) + 1
             while is_valid(hmap, tmap):
-                if r-l+1 < minlen:
-                    minlen = r-l+1
-                    res = [l,r+1]
-                if s[l] in hmap:
-                    hmap[s[l]] -= 1
+                if res > r-l+1:
+                    res = r-l+1
+                    sol[0] = l
+                    sol[1] = r+1
+                hmap[s[l]] -= 1
                 l += 1
-
-        return s[res[0]:res[1]]
-
+        return s[sol[0]: sol[1]]
 
