7834594faab44510f3a395165fb542b2ee11149b
Vishay Raina <46987787+vishayraina@users.noreply.github.com>
2026-08-04 16:47:20 +0530

    Time: 661 ms (5.07%), Space: 49 MB (79.04%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.py
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.py
@@ -1,7 +1,22 @@
 class Solution:
     def eraseOverlapIntervals(self, intervals: List[List[int]]) -> int:
-        def lower_bound(l, r, target):
-            res = len(intervals)
+        # need last non overlapping interval p[i] for each interval i
+        # i.e., p[i][1] <= intervals[i][0]
+        # i.e., the largest end time that is less than current interval start time. lowerbound + 1
+
+        # dp[n] = max(dp[n-1], dp[p[n]]+1)
+
+        # dp[n] := max non overlapping intervals given the first n intervals
+        intervals.sort(key = lambda x: x[1])
+        def p(i):
+            # l, r = 0, i-1
+            # target = intervals[i][0]
+            # while l <= r:
+            #     mid = (l+r)//2
+            #     if target < intervals[mid]:
+            l, r = 0, i-1
+            target = intervals[i][0]
+            res = i
             while l <= r:
                 mid = (l+r)//2
                 if target < intervals[mid][1]:
@@ -9,11 +24,12 @@ class Solution:
                     r = mid - 1
                 else:
                     l = mid + 1
-            return res-1
+            return res
 
-        intervals.sort(key = lambda x: x[1])
-        dp = [0]*(len(intervals)+1)
+        dp = [0] * (len(intervals)+1)
         for i in range(1, len(dp)):
-            last = lower_bound(0, i-1, intervals[i-1][0])
-            dp[i] = max(dp[i-1], dp[last+1]+1)
-        return len(intervals) - dp[-1] 
+            dp[i] = max(dp[i-1], dp[p(i-1)]+1)
+        print(dp)
+        return  len(intervals) - dp[-1]
+
+            
