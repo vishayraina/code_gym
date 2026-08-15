961ab41131a54d643240bce752ef22a9abad2bc9
Vishay Raina <46987787+vishayraina@users.noreply.github.com>
2026-08-15 17:47:19 +0530

    Time: 640 ms (5.1%), Space: 49.1 MB (55.68%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.py
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.py
@@ -1,29 +1,30 @@
 class Solution:
     def eraseOverlapIntervals(self, intervals: List[List[int]]) -> int:
-        # need last non overlapping interval p[i] for each interval i
-        # i.e., p[i][1] <= intervals[i][0]
-        # i.e., the largest end time that is less than current interval start time. lowerbound + 1
-
-        # dp[n] = max(dp[n-1], dp[p[n]]+1)
-
-        # dp[n] := max non overlapping intervals given the first n intervals
-        intervals.sort(key = lambda x: x[1])
-        def p(i):
-            l, r = 0, i-1
-            target = intervals[i][0]
-            res = i
-            while l <= r:
-                mid = (l+r)//2
-                if target < intervals[mid][1]:
-                    res = mid
-                    r = mid - 1
-                else:
-                    l = mid + 1
+        intervals.sort( key = lambda x: x[1])
+        def get_prev_interval(idx):
+            res = -1
+            for i in range(idx-1, -1, -1):
+                if intervals[idx][0] >= intervals[i][1]:
+                    res = i
+                    break
             return res
 
-        dp = [0] * (len(intervals)+1)
-        for i in range(1, len(dp)):
-            dp[i] = max(dp[i-1], dp[p(i-1)]+1)
-        return  len(intervals) - dp[-1]
+        def get_floor(l, r, target):
+            # largest element <= target
+            res = -1
+            while l<=r:
+                mid = (l+r)//2
+                if intervals[mid][1] <= target:
+                    res = mid
+                    l = mid+1
+                else:
+                    r = mid - 1
+            return res
+            return res
 
-            
+        dp = [0]*(len(intervals)+1)
+        for i in range(1, len(dp)):
+            # p = get_prev_interval(i-1)
+            p = get_floor(0, i-2, intervals[i-1][0])
+            dp[i] = max(dp[i-1], dp[p+1]+1)
+        return len(intervals)-dp[-1]
