736e201f372953a4d1d21ee4d567fe9fed4e0c31
Vishay Raina <46987787+vishayraina@users.noreply.github.com>
2026-08-15 17:48:09 +0530

    Time: 709 ms (5.1%), Space: 49 MB (78.98%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.py
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.py
@@ -1,14 +1,6 @@
 class Solution:
     def eraseOverlapIntervals(self, intervals: List[List[int]]) -> int:
         intervals.sort( key = lambda x: x[1])
-        def get_prev_interval(idx):
-            res = -1
-            for i in range(idx-1, -1, -1):
-                if intervals[idx][0] >= intervals[i][1]:
-                    res = i
-                    break
-            return res
-
         def get_floor(l, r, target):
             # largest element <= target
             res = -1
@@ -23,7 +15,6 @@ class Solution:
 
         dp = [0]*(len(intervals)+1)
         for i in range(1, len(dp)):
-            # p = get_prev_interval(i-1)
             p = get_floor(0, i-2, intervals[i-1][0])
             dp[i] = max(dp[i-1], dp[p+1]+1)
         return len(intervals)-dp[-1]
